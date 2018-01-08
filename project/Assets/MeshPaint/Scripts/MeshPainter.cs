053878ba15c51a91f3b20e3e45bb72d1430fc558
daao <a1294246096@gmail.com>
2018-01-08 23:58:00 +0800

    逆天

---
+++ project/Assets/MeshPaint/Scripts/MeshPainter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+//https://zhuanlan.zhihu.com/myashader
+//https://www.gamasutra.com/blogs/AndreyMishkinis/20130716/196339/Advanced_Terrain_Texture_Splatting.php
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshCollider))]
+public class MeshPainter : MonoBehaviour {
+
+    void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+
+}
---
+++ project/Assets/test.cs
@@ -0,0 +1,31 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class test : MonoBehaviour {
+//    public static test instance;
+//    // Use this for initialization
+//    void Start () {
+//        instance = this;
+//    }
+//    bool start = false;
+//    public Texture2D MaskTex;
+//    // Update is called once per frame
+//    void Update () {
+//        if (start)
+//        {
+//            start = false;
+//            SaveTexture();
+//        }
+//    }
+
+//    public void SaveTexture()
+//    {
+//        var path = AssetDatabase.GetAssetPath(MaskTex);
+//        var bytes = MaskTex.EncodeToPNG();
+//        File.WriteAllBytes(path, bytes);
+//        AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);//刷新
+
+//        UnityEngine.Debug.Log("SaveTexture");
+//    }
+//}
